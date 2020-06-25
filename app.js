d844e0cf896475a086ebee9dba159defa02a33e4
gergely-sajo <sajo.gergo@gmail.com>
2020-06-25 08:11:42 +0200

    Finished register data logic, register data validation

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 
 const router = require('./router')
 
+app.use(express.urlencoded({extended: false})) // have toconfigure the express app for the userController to be able to understand the input. It basically only tells express to add the user-submitted data onto our request object
+app.use(express.json())
+
 app.use(express.static('public'))
 app.set('views', 'views') // the first argument has to be 'views', but the second argument should be the name of the folder of our view files, .ejs files
 app.set('view engine', 'ejs') // we have to let express know which template engine or templating system we are using
---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User')
+
+exports.login = function() {
+
+}
+
+exports.logout = function() {
+    
+}
+
+exports.register = function(req, res) {
+    let user = new User(req.body)
+    user.register()
+    if (user.errors.length) {
+        res.send(user.errors)
+    } else {
+        res.send("Success")
+    }
+}
+
+exports.home = function(req, res) {
+    res.render('home-guest')
+}
