e50bcd793d05bb8125283a6b071d98cf20b15592
gergely-sajo <sajo.gergo@gmail.com>
2020-06-28 11:07:54 +0200

    Improved register functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,18 +23,25 @@ exports.logout = function(req, res) {
 
 exports.register = function(req, res) {
     let user = new User(req.body)
-    user.register()
-    if (user.errors.length) {
-        res.send(user.errors)
-    } else {
-        res.send("Success")
-    }
+    user.register().then(() => {
+        req.session.user = {username: user.data.username}
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    }).catch((regErrors) => {
+        regErrors.forEach(function(error) {
+            req.flash('regErrors', error)
+        })
+        req.session.save(function() {
+            res.redirect('/')
+        })
+    })
 }
 
 exports.home = function(req, res) {
     if (req.session.user) {
         res.render('home-dashboard', {username: req.session.user.username})
     } else {
-        res.render('home-guest', {errors: req.flash('errors')})
+        res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
     }
 }
--- models/User.js
+++ models/User.js
@@ -21,17 +21,33 @@ User.prototype.cleanUp = function() {
 }
 
 User.prototype.validate = function() {
+    return new Promise(async (resolve, reject) => {
 
-    if (this.data.username == "") {this.errors.push("You must provide a username!")}
-    if (this.data.username.length > 0 && this.data.username.length < 3) {this. errors.push("Username must be at least 12 characters.")}
-    if (this.data.username.length > 30) {this. errors.push("Username cannot exceed 30 characters.")}
-    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+        if (this.data.username == "") {this.errors.push("You must provide a username!")}
+        if (this.data.username.length > 0 && this.data.username.length < 3) {this. errors.push("Username must be at least 12 characters.")}
+        if (this.data.username.length > 30) {this. errors.push("Username cannot exceed 30 characters.")}
+        if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
+    
+        // Only if username is valid, then check to see if its already taken
+        if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+            let usernameExists = await usersCollection.findOne({username: this.data.username})
+            if (usernameExists) {this.errors.push("That username is already taken.")}
+        }
+    
+        if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address!")}
+    
+        // Only if email is valid, then check to see if its already taken
+        if (validator.isEmail(this.data.email)) {
+            let emailExists = await usersCollection.findOne({email: this.data.email})
+            if (emailExists) {this.errors.push("That email has already been registered.")}
+        }
+    
+        if (this.data.password == "") {this.errors.push("You must provide a password!")}
+        if (this.data.password.length > 0 && this.data.password.length < 12) {this. errors.push("Password must be at least 12 characters.")}
+        if (this.data.password.length > 50) {this. errors.push("Password cannot exceed 50 characters.")}
 
-    if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address!")}
-
-    if (this.data.password == "") {this.errors.push("You must provide a password!")}
-    if (this.data.password.length > 0 && this.data.password.length < 12) {this. errors.push("Password must be at least 12 characters.")}
-    if (this.data.password.length > 50) {this. errors.push("Password cannot exceed 50 characters.")}
+        resolve()
+    })
 }
 
 User.prototype.login = function() {
@@ -50,18 +66,22 @@ User.prototype.login = function() {
 }
 
 User.prototype.register = function() {
-    // Step #1: Validate user data
-    this.cleanUp()
-    this.validate()
-
-    // Step #2: Only if there are no validation errors, then safe the data into a database
-    if (!this.errors.length) {
-        // Hash user passwords
-        let salt = bcrypt.genSaltSync(10)
-        this.data.password = bcrypt.hashSync(this.data.password, salt)
-
-        usersCollection.insertOne(this.data)
-    }
+    return new Promise(async (resolve, reject) => {
+        // Step #1: Validate user data
+        this.cleanUp()
+        await this.validate()
+    
+        // Step #2: Only if there are no validation errors, then safe the data into a database
+        if (!this.errors.length) {
+            // Hash user passwords
+            let salt = bcrypt.genSaltSync(10)
+            this.data.password = bcrypt.hashSync(this.data.password, salt)
+            await usersCollection.insertOne(this.data)
+            resolve()
+        } else {
+            reject(this.errors)
+        }
+    })
 }
 
 module.exports = User
