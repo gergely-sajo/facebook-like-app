048d659050da2600880d081861c312b3fcd8dde2
gergely-sajo <sajo.gergo@gmail.com>
2020-06-30 08:09:57 +0200

    Added posting and post viewing functionalities

--- router.js
+++ router.js
@@ -4,13 +4,14 @@ const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 
 // User Related Routes
-
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
 // Post Related Routes
-router.get('/create-post',userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
+router.get('/post/:id', postController.viewSingle)
 
 module.exports = router
