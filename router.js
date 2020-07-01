41931b04a43055e89f8b8801baadfe366d60959d
gergely-sajo <sajo.gergo@gmail.com>
2020-07-01 12:05:26 +0200

    Added user profile page and functionality, without viewing the correct user profile posts. That comes next. For now the username and user profile picture(avatar) is correct. If we go to a not existing user profile page we get a 404 page.

--- router.js
+++ router.js
@@ -9,6 +9,9 @@ router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
+// Profile Related Routes
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
+
 // Post Related Routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
