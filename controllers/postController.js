5c75af915e5e8eecb36d31fee9f3c46ebfb0e53b
gergely-sajo <sajo.gergo@gmail.com>
2020-07-02 07:53:38 +0200

    Completed edit post funtionality. Added flash messages to every page, relocated the errors and success flash messages to the app.js file so they are accessable from every template.

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,9 +6,9 @@ exports.viewCreateScreen = function(req, res) {
 
 exports.create = function(req, res) {
     let post = new Post(req.body, req.session.user._id)
-    post.create().then(function() {
+    post.create().then(() => {
         res.send("New post created.")
-    }).catch(function(errors) {
+    }).catch((errors) => {
         res.send(errors)
     })
 }
@@ -20,4 +20,42 @@ exports.viewSingle = async function(req, res) {
     } catch {
         res.render('404')
     }
+}
+
+exports.viewEditScreen = async function(req, res) {
+    try {
+        let post = await Post.findSingleById(req.params.id)
+        res.render('edit-post', {post: post})
+    } catch {
+        res.render('404')
+    }
+}
+
+exports.edit = function(req, res) {
+    let post = new Post(req.body, req.visitorId, req.params.id)
+    post.update().then((status) => {
+        if (status = "success") {
+            // post was updated in the database
+            req.flash("success", "Post successfully updated.")
+            req.session.save(() => {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        } else {
+            // there were validation errors
+            post.errors.forEach((error) => {
+                req.flash("errors", error)
+            })
+            req.session.save(() => {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        }
+
+    }).catch(() => {
+        // a post with the requested id doesnt exist
+        // or the current visitor is not the owner of the requested post
+        req.flash("errors", "You do not have permission to edit this post!")
+        req.session.save(() => {
+            res.redirect('/')
+        })
+    })
 }
--- models/Post.js
+++ models/Post.js
@@ -2,10 +2,11 @@ const postsCollection = require('../db').db().collection("posts")
 const ObjectID = require('mongodb').ObjectID
 const User = require('./User')
 
-let Post = function(data, userid) {
-    this.data = data,
-    this.errors = [],
+let Post = function(data, userid, requestedPostId) {
+    this.data = data
+    this.errors = []
     this.userid = userid
+    this.requestedPostId = requestedPostId
 }
 
 Post.prototype.cleanUp = function() {
@@ -40,6 +41,36 @@ Post.prototype.create = function() {
             })
         } else {
             reject(this.errors)
+        }
+    })
+}
+
+Post.prototype.update = function() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let post = await Post.findSingleById(this.requestedPostId, this.userid)
+            if (post.isVisitorOwner) {
+                // update db
+                let status = await this.actuallyUpdate()
+                resolve(status)
+            } else {
+                reject()
+            }
+        } catch {
+            reject()
+        }
+    })
+}
+
+Post.prototype.actuallyUpdate = function() {
+    return new Promise(async (resolve, reject) => {
+        this.cleanUp()
+        this.validate()
+        if (!this.errors.length) {
+            await postsCollection.findOneAndUpdate({_id: new ObjectID(this.requestedPostId)}, {$set: {title: this.data.title, body: this.data.body}})
+            resolve("success")
+        } else {
+            resolve("failure")
         }
     })
 }
