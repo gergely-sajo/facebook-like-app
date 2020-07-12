8f41c8d23892c312b906fec5accce2c95a81f52b
gergely-sajo <sajo.gergo@gmail.com>
2020-07-12 10:17:23 +0200

    Configured following tab and logic on profile pages.

--- models/Follow.js
+++ models/Follow.js
@@ -103,4 +103,28 @@ Follow.getFollowersById = function(id) {
     })
 }
 
+Follow.getFollowingById = function(id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let following = await followsCollecion.aggregate([
+                {$match: {authorId: id}},
+                {$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
+                {$project: {
+                    username: {$arrayElemAt: ["$userDoc.username", 0]},
+                    email: {$arrayElemAt: ["$userDoc.email", 0]}
+                }}
+            ]).toArray()
+
+            following = following.map(function(following) {
+                let user = new User(following, true)
+                return {username: following.username, avatar: user.avatar}
+            })
+
+            resolve(following)
+        } catch {
+            reject()
+        }
+    })
+}
+
 module.exports = Follow
--- router.js
+++ router.js
@@ -13,8 +13,7 @@ router.post('/logout', userController.logout)
 // Profile Related Routes
 router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
 router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
-
-
+router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen)
 
 // Post Related Routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
