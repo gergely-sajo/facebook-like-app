8bdc20e9f86d8ab8b01d87544bb308817f5476ce
gergely-sajo <sajo.gergo@gmail.com>
2020-07-17 06:57:46 +0200

    Modified some file to be able to push the app onto heroku.

--- router-api.js
+++ router-api.js
@@ -1,7 +1,10 @@
 const apiRouter = require('express').Router()
+const cors = require('cors') // Cross Origin Resource Sharing. 
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 const followController = require('./controllers/followController')
+
+apiRouter.use(cors()) // We have to explicitly say that it is okay to receive api requests. Webbrowser doesnt send off asynchronous requests to other domains unless that other domain explicitly say its okay 
 
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
